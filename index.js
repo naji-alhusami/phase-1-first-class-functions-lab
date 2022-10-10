ea75e7908b4c7cb2c0038ba0c6ca444c2f45e599
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-10 17:03:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = () => {
+    return function (num) {
+        return num*num;
+    }
+}
+
+const fareDoubler = (num) => {
+ return num*2;
+}
+
+
+const fareTripler = (num) => {
+    return num*3;
+}
+
+const selectDifferentDrivers = (drivers, function_drivers) => {
+    return function_drivers(drivers);
+}
